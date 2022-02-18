9bf72e671ee659059fc88f652630269cfa4ef870
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-17 20:37:34 -0500

    add/ basic types

--- src/typescript/BasicTypes.tsx
+++ src/typescript/BasicTypes.tsx
@@ -1,8 +1,21 @@
 
 export const BasicTypes = () => {
+
+  const name: string | number = 'Jorge'; // Optional types to variable
+  const age: number = 35;
+  const isActive: boolean = true;
+  const powers = [".",".",".",".",];
+  const powersTwo: string[] = [".",".",".",".",];
+
   return (
     <>
       <h3>BasicTypes</h3>
+      { name }, 
+      { age }, 
+      { JSON.stringify(isActive) }, 
+      { isActive ? 'is Active' : 'No active' }
+      { powers.join(' ') }
+      { powersTwo }
     </>
   )
-}
+};
