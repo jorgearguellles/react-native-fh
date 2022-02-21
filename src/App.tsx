b31d6bf74e1112183f54f80e421810e44a562b1e
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-21 11:13:17 -0500

    add/ nextPage, backPage, pagination logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Login } from './components/Login';
+import { Users } from './components/Users';
 
 function App() {
 
@@ -6,7 +6,7 @@ function App() {
     <div className="mt-2">
       <h1>React Native</h1>
       <hr />
-      <Login />
+      <Users />
       
     </div>
   );
